f6852f525fe13eb4744fe4d36ba19b13cdf317d2
AnthonyEnr1quez <32233059+AnthonyEnr1quez@users.noreply.github.com>
2022-12-02 17:43:35 -0600

    "optimize" Rock Paper Scissors

--- 2022/02/main.go
+++ 2022/02/main.go
@@ -16,6 +16,7 @@ import (
 // plus the score for the outcome of the round
 // (0 if you lost, 3 if the round was a draw, and 6 if you won).
 
+// https://adventofcode.com/2022/day/2
 func main() {
 	file, err := os.Open("input.txt")
 	defer file.Close()
@@ -35,78 +36,66 @@ func main() {
 		oppMove := vals[0]
 		strat := vals[1]
 
+		// Correct
 		// A for Rock, B for Paper, and C for Scissors (opp)
-		// X means you need to lose,
-		// Y means you need to end the round in a draw,
-		// Z means you need to win.
-		// part 2
-		switch oppMove {
-		case "A":
-			if strat == "X" {
-				//scissors
-				correctStratScore += 3
-
-			} else if strat == "Y" {
-				// rock
-				correctStratScore += 1
-				correctStratScore += 3
-			} else if strat == "Z" {
-				// paper
-				correctStratScore += 2
-				correctStratScore += 6
-			}
-		case "B":
-			if strat == "X" {
-				// rock
-				correctStratScore += 1
-			} else if strat == "Y" {
-				// paper
-				correctStratScore += 2
-				correctStratScore += 3
-			} else if strat == "Z" {
-				// scissors
-				correctStratScore += 3
-				correctStratScore += 6
-			}
-		case "C":
-			if strat == "X" {
-				// paper
-				correctStratScore += 2
-			} else if strat == "Y" {
-				// scissors
-				correctStratScore += 3
-				correctStratScore += 3
-			} else if strat == "Z" {
-				// rock
-				correctStratScore += 1
-				correctStratScore += 6
-			}
-		}
-
-		// A for Rock, B for Paper, and C for Scissors (opp)
+		// X for lose, Y for draw, and Z for win
+		// Incorrect
 		// X for Rock, Y for Paper, and Z for Scissors (me)
-		// part 1
 		switch strat {
 		case "X":
 			incorrectStratScore++
-			if oppMove == "A" {
+			switch oppMove {
+			case "A":
+				//scissors
 				incorrectStratScore += 3
-			} else if oppMove == "C" {
+				correctStratScore += 3
+			case "B":
+				// rock
+				correctStratScore += 1
+			case "C":
+				// paper
 				incorrectStratScore += 6
+				correctStratScore += 2
 			}
 		case "Y":
 			incorrectStratScore += 2
-			if oppMove == "A" {
+			switch oppMove {
+			case "A":
+				// rock
 				incorrectStratScore += 6
-			} else if oppMove == "B" {
+
+				correctStratScore += 1
+				correctStratScore += 3
+			case "B":
+				// paper
 				incorrectStratScore += 3
+
+				correctStratScore += 2
+				correctStratScore += 3
+			case "C":
+				// scissors
+				correctStratScore += 3
+				correctStratScore += 3
 			}
 		case "Z":
 			incorrectStratScore += 3
-			if oppMove == "B" {
+			switch oppMove {
+			case "A":
+				// paper
+				correctStratScore += 2
+				correctStratScore += 6
+			case "B":
+				// scissors
 				incorrectStratScore += 6
-			} else if oppMove == "C" {
+
+				correctStratScore += 3
+				correctStratScore += 6
+			case "C":
+				// rock
 				incorrectStratScore += 3
+
+				correctStratScore += 1
+				correctStratScore += 6
 			}
 		}
 	}
