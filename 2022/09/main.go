9b47743925aa410a20623a912e93ebcdf63250bf
AnthonyEnr1quez <32233059+AnthonyEnr1quez@users.noreply.github.com>
2022-12-14 11:12:16 -0600

    Rope Bridge refactor to remove grid

--- 2022/09/main.go
+++ 2022/09/main.go
@@ -27,97 +27,52 @@ func main() {
 		motions = append(motions, Motion{Direction: motion[0], Steps: steps})
 	}
 
-	xCord := 600
-	yCord := 600
-	var grid Grid
-	grid = make([][]*Point, yCord)
-	for y := range grid {
-		grid[y] = make([]*Point, xCord)
-		for x := 0; x < xCord; x++ {
-			grid[y][x] = &Point{
-				X: x,
-				Y: y,
-			}
-		}
-	}
+	head := Point{X: 0, Y: 0}
+	tail := Point{X: 0, Y: 0}
 
-	head := grid[300][300]
-	tail := head
+	visited := make(map[Point]struct{})
 
 	for _, motion := range motions {
 		for i := 1; i <= motion.Steps; i++ {
-			tail.Visited = true
-
 			switch motion.Direction {
 			case "U":
 				// Tail below Head
 				if tail.Y < head.Y {
 					tail = head
-					tail.Visited = true
 				}
-				head = grid[head.Y+1][head.X]
+				head.Y++
 			case "D":
 				// Tail above Head
 				if tail.Y > head.Y {
 					tail = head
-					tail.Visited = true
 				}
-				head = grid[head.Y-1][head.X]
+				head.Y--
 			case "R":
 				// Tail left of Head
 				if tail.X < head.X {
 					tail = head
-					tail.Visited = true
 				}
-				head = grid[head.Y][head.X+1]
+				head.X++
 			case "L":
 				// Tail right of Head
 				if tail.X > head.X {
 					tail = head
-					tail.Visited = true
 				}
-				head = grid[head.Y][head.X-1]
+				head.X--
 			}
+
+			visited[tail] = struct{}{}
 		}
 	}
 
-	fmt.Println(grid.Visited(xCord, yCord))
+	fmt.Println(len(visited))
 }
 
-type Grid [][]*Point
-
 type Point struct {
-	X, Y    int
-	Visited bool
+	X, Y int
 }
 
 type Motion struct {
 	Direction string
 	Steps     int
-}
-
-func (g Grid) Print(xCord, yCord int, head, tail *Point) {
-	for y := yCord - 1; y >= 0; y-- {
-		for x := 0; x < xCord; x++ {
-			if g[y][x] == head {
-				fmt.Print("H")
-			} else if g[y][x] == tail {
-				fmt.Print("T")
-			} else {
-				fmt.Print(".")
-			}
-		}
-		fmt.Println()
-	}
-}
-
-func (g Grid) Visited(xCord, yCord int) (visited int) {
-	for y := yCord - 1; y >= 0; y-- {
-		for x := 0; x < xCord; x++ {
-			if g[y][x].Visited {
-				visited++
-			}
-		}
-	}
-	return
 }
